d7371bfd62940329e78589672222c9038814a459
Clarence "Sparr" Risher <sparr0@gmail.com>
2022-01-26 23:51:22 -0500

    Initial Commit - buggy

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quick Switcher More Channels
+
+Adds favorite and/or recent channels to the Quick Switcher
+
+## Features
+
+Hover over a channel name for an icon to favorite or un-favorite the channel.
+
+See favorites and/or most recently active channels in the quick switcher.
+
+## Installation
+
+1. Clone this repository into your Powercord plugins folder using `git clone https://github.com/sparr/powercord-quick-switcher-more-channels.git`
+2. Reload Discord using `Ctrl + R`
---
+++ index.js
@@ -0,0 +1,140 @@
+const { Plugin } = require("powercord/entities");
+const { Icon } = require("powercord/components");
+const { inject, uninject } = require("powercord/injector");
+const {
+  React,
+  getModule,
+  getModuleByDisplayName,
+  i18n: { Messages },
+} = require("powercord/webpack");
+const i18n = require("./i18n");
+const { getChannel } = getModule(["getMutableGuildChannels"], false);
+
+module.exports = class QuickSwitcherMoreChannels extends Plugin {
+  async startPlugin() {
+    powercord.api.i18n.loadAllStrings(i18n);
+
+
+    //FIXME: stop doing this when things start working
+    this.settings.set("favorite-channels", new Set());
+
+    
+    const QuickSwitcher = getModule(
+      (m) => m?.default?.displayName === "QuickSwitcherConnected",
+      false
+    );
+
+    // add Favorite Channels to the default quick switcher results
+    inject(
+      "quick-switcher-more-channels-quickswitcher",
+      QuickSwitcher,
+      "default",
+      (_, res) => {
+        if (res.props.query != "") return res;
+        console.log(res);
+        let index = 0;
+        // Favorite Channels goes after Last Channel if present
+        if (
+          res.props.results.length > 0 &&
+          res.props.results[0].record.id === "Last Channel"
+        )
+          index = 2;
+        if (
+          res.props.results.length > index &&
+          res.props.results[index].record.id === "Favorite Channels"
+        )
+          return res;
+        const favorites = this.settings.get("favorite-channels");
+        if (favorites && favorites.size > 0) {
+          let favorites_results = Array.from(favorites, (id) => {
+            let channel = getChannel(id.toString());
+            return {
+              comparator: channel.name,
+              record: channel,
+              score: 0,
+              type: "TEXT_CHANNEL",
+            };
+          });
+          favorites_results.unshift({
+            record: {
+              id: "Favorite Channels",
+              text: Messages.QUICKSWITCHER_FAVORITE_CHANNELS,
+            },
+            score: 0,
+            type: "HEADER",
+          });
+          res.props.results.splice(index, 0, ...favorites_results);
+        }
+        return res;
+      }
+    );
+
+
+    const iconClasses = await getModule(["iconItem"]);
+    const Tooltip = await getModuleByDisplayName("Tooltip");
+    const ChannelItem = await getModule(
+      (m) => m.default && m.default.displayName == "ChannelItem"
+    );
+    const mod = getModule(["updateChannelOverrideSettings"], false);
+
+    // Add a hover icon to channels in the channel list, to fave/unfave
+    inject(
+      "quick-switcher-more-channels-channelitem",
+      ChannelItem,
+      "default",
+      (args) => {
+        if (args.length == 0) return args;
+        if (args[0]["channel"]["type"] == 2) return args;
+        let favorites = this.settings.get("favorite-channels");
+        const favorited =
+          favorites && favorites.size > 0
+            ? favorites.has(args[0]["channel"]["id"])
+            : false;
+        console.log(favorited);
+        args[0].children.unshift(
+          React.createElement(
+            "div",
+            { className: iconClasses.iconItem },
+            React.createElement(
+              Tooltip,
+              {
+                text: favorited
+                  ? Messages.REMOVE_FAVORITE_CHANNEL
+                  : Messages.ADD_FAVORITE_CHANNEL,
+              },
+              (props) =>
+                React.createElement(Icon, {
+                  ...props,
+                  name: favorited ? "StarOff" : "Star",
+                  className: iconClasses.actionIcon,
+                  width: 16,
+                  height: 16,
+                  onClick: () => {
+                    if (!favorited) {
+                      console.log(favorites);
+                      favorites.add(args[0]["channel"]["id"]);
+                      this.settings.set("favorite-channels", favorites);
+                      console.log(favorites);
+                    } else {
+                      console.log(favorites);
+                      favorites.remove(args[0]["channel"]["id"]);
+                      this.settings.set(favoritechannels, favorites);
+                      console.log(favorites);
+                    }
+                  },
+                })
+            )
+          )
+        );
+        return args;
+      },
+      true
+    );
+
+    QuickSwitcher.default.displayName = "QuickSwitcherConnected";
+  }
+
+  pluginWillUnload() {
+    uninject("quick-switcher-more-channels`");
+  }
+};
